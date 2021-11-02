322d8071f64f69abd884150b8a2c507350853af1
Stephen Wong <stephen_wong_tx@icloud.com>
2021-11-02 18:58:06 -0500

    first commit

--- script/seed.js
+++ script/seed.js
@@ -13,7 +13,7 @@ async function seed() {
   // Creating Users
   const users = await Promise.all([
     User.create({ username: 'cody', password: '123' }),
-    User.create({ username: 'murphy', password: '123' }),
+    User.create({ username: 'bill', password: '123' }),
   ])
 
   console.log(`seeded ${users.length} users`)
